8ee6e97dce89ba275101a18583829b253943321a
IIIristraM <iiiristram@DESKTOP-0TRSO1A.localdomain>
2023-05-28 01:29:39 +0300

    redis

---
+++ db/redis/README.md
@@ -0,0 +1,1 @@
+Create `data` subfolder in this folder before `npm start`
--- services/queries-processor/src/index.ts
+++ services/queries-processor/src/index.ts
@@ -1,14 +1,28 @@
 import * as http from 'http';
+import { createClient } from 'redis';
+
+const client = createClient({
+    url: 'redis://redis:6379'
+});
 
 const hostname = '0.0.0.0';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Queries processor');
-});
+async function main() {
+    await client.connect();
+    await client.set('key', 'value');
+    const value = await client.get('key');
+    console.log('REDIS', value)
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+    const server = http.createServer((req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Queries processor');
+    });
+
+    server.listen(port, hostname, () => {
+        console.log(`Server running at http://${hostname}:${port}/`);
+    });
+}
+
+main();
