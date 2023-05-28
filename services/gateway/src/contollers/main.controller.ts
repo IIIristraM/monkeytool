c18aa01756142c03609e25ba13433719170ebc2b
IIIristraM <iiiristram@DESKTOP-0TRSO1A.localdomain>
2023-05-28 23:10:03 +0300

    mongoose

--- services/gateway/src/contollers/main.controller.ts
+++ services/gateway/src/contollers/main.controller.ts
@@ -1,15 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 
 import { QueriesService } from '../services';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 @Controller()
 export class MainController {
   constructor(
     private readonly queriesService: QueriesService,
+    @InjectConnection() private readonly connection: Connection
   ) {}
 
   @Get()
-  ping() {
+  async ping() {
+    console.log(await this.connection.db.command({ ping: 1 }));
     return this.queriesService.execute();
   }
 }
--- services/gateway/src/modules/main.module.ts
+++ services/gateway/src/modules/main.module.ts
@@ -1,25 +1,18 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
-import { MongoClient  } from 'mongodb'
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { MainController } from '../contollers';
 import { QueriesService } from '../services';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule,
+    MongooseModule.forRoot('mongodb://root:example@mongo:27017')
+  ],
   controllers: [MainController],
   providers: [
       QueriesService,
-      {
-        provide: 'MONGO',
-        useFactory: async () => {
-            const connectionStr = 'mongodb://root:example@mongo:27017/admin';
-            const mongoClient = new MongoClient(connectionStr);
-            await mongoClient.connect();
-            await mongoClient.db().command({ping: 1});
-            return mongoClient;
-        }
-      }
     ],
 })
 
